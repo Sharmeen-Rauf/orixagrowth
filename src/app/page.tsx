dedaae46a35a3264e18eb403405eb010619ce014
Sharmeen-Rauf <sharmeenpakistan8@gmail.com>
2026-02-04 22:19:22 +0500

    Reintroduce MarketingStatsSection in the main page layout, enhancing the overall structure and user engagement.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,9 @@ export default function Home() {
     <>
       <Navbar />
       <Hero />
-      <MarketingStatsSection />
       <WavySeparator />
       <ServicesSection />
+      <MarketingStatsSection />
       <WhatsAppButton />
       <ScrollToTop />
     </>
