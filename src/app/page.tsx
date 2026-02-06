346c2c924b2315809b8b14f97dbb31f5567467b0
Sharmeen-Rauf <sharmeenpakistan8@gmail.com>
2026-02-06 20:49:28 +0500

    Add FeaturesIllustrationSection and FeaturesApproachSection to main page layout for enhanced feature presentation and user engagement.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import MarketingStatsSection from "@/components/MarketingStatsSection";
 import WhyBrandsTrustSection from "@/components/WhyBrandsTrustSection";
+import FeaturesIllustrationSection from "@/components/FeaturesIllustrationSection";
+import FeaturesApproachSection from "@/components/FeaturesApproachSection";
 import ServicesSection from "@/components/ServicesSection";
 import WhatsAppButton from "@/components/WhatsAppButton";
 import ScrollToTop from "@/components/ScrollToTop";
@@ -14,6 +16,8 @@ export default function Home() {
       <ServicesSection />
       <MarketingStatsSection />
       <WhyBrandsTrustSection />
+      <FeaturesIllustrationSection />
+      <FeaturesApproachSection />
       <WhatsAppButton />
       <ScrollToTop />
     </>
